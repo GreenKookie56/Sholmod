3b168caa570f01faf7873a8ed63388568e522c66
GreenKookie56 <brandon05121@gmail.com>
2025-11-16 09:36:03 +0800

    Add files via upload

--- jokers/bloonchipper.lua
+++ jokers/bloonchipper.lua
@@ -4,6 +4,7 @@ SMODS.Joker{ --Bloonchipper
         extra = {
             discard = 0,
             discardmod = 1,
+            odds = 2,
             round = 0
         }
     },
@@ -12,7 +13,8 @@ SMODS.Joker{ --Bloonchipper
         ['text'] = {
             [1] = '{C:red}+#1#{} discard(s)',
             [2] = 'when {C:attention}Blind{} is selected',
-            [3] = 'Increase by {C:red}+#2#{} each {C:attention}Ante{}'
+            [3] = '{C:green}#4# in #5#{} chance to increase by',
+            [4] = '{C:red}+#2#{} each {C:attention}Ante{}'
         },
         ['unlock'] = {
             [1] = ''
@@ -37,32 +39,25 @@ SMODS.Joker{ --Bloonchipper
     atlas = 'CustomJokers',
 
     loc_vars = function(self, info_queue, card)
-        return {vars = {card.ability.extra.discard, card.ability.extra.discardmod}}
+        local new_numerator, new_denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, 'j_sholium_bloonchipper') 
+        return {vars = {card.ability.extra.discard, card.ability.extra.discardmod, card.ability.extra.round, new_numerator, new_denominator}}
     end,
 
     calculate = function(self, card, context)
-        if context.end_of_round and context.main_eval and G.GAME.blind.boss  and not context.blueprint then
-                return {
-                    func = function()
+        if context.end_of_round and context.main_eval and G.GAME.blind.boss  then
+            if true then
+                if SMODS.pseudorandom_probability(card, 'group_0_0bf57d31', 1, card.ability.extra.odds, 'j_sholium_bloonchipper', false) then
+              SMODS.calculate_effect({func = function()
                     card.ability.extra.discard = (card.ability.extra.discard) + card.ability.extra.discardmod
                     return true
-                end,
-                    message = "Upgrade!"
-                }
+                end}, card)
+                        card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = "Upgrade!", colour = G.C.GREEN})
+          end
+            end
         end
-        if context.setting_blind  then
+        if context.setting_blind or context.forcetrigger then
                 return {
                     func = function()
-                card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = "+"..tostring(card.ability.extra.discard).." Discard", colour = G.C.ORANGE})
-                G.GAME.current_round.discards_left = G.GAME.current_round.discards_left + card.ability.extra.discard
-                return true
-            end
-                }
-        end
-        if context.forcetrigger then
-          return {
-            func = function()
-                card.ability.extra.discard = (card.ability.extra.discard) + card.ability.extra.discardmod    
                 card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = "+"..tostring(card.ability.extra.discard).." Discard", colour = G.C.ORANGE})
                 G.GAME.current_round.discards_left = G.GAME.current_round.discards_left + card.ability.extra.discard
                 return true
